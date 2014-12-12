47b30bd7bf1e89c2bfdc5d5613632c402de6dc8d
zaim ramlan <zaem.muz@gmail.com>
2014-12-13 01:57:01 +0800

    Reorganized javascript files and added menu transparency with slide anim.

---
+++ src/main/webapp/custom.js
@@ -0,0 +1,8 @@
+$(document).ready(dot);
+
+function dot() {
+	$('.trig').click(function(){
+		// $('#map-canvas').toggle("drop",{direction:'up'});
+		$('#right-col').toggle("drop",{direction:'right'});
+	});
+}
